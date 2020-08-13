53854ceab0f388c56b4532de52bb307e8c263bd8
Magnus Buk <info@magnusbuk.de>
2020-08-13 10:23:07 +0200

    Template fix

--- resources/views/visit/index.blade.php
+++ resources/views/visit/index.blade.php
@@ -21,10 +21,10 @@
                 <div class="flex justify-center">
                     <img src="/img/qr/{{ $participant->secret }}.jpg"/>
                 </div>
-                <div class="text-center text-xs text-gray-600">
+                <div class="text-center text-xs text-gray-600 mt-3">
                     <b>Achtung:</b> Nur durch Vorzeigen des QR-Codes können wir Deinen Einlass garantieren. Speichere ihn daher auf Deinem Handy ab oder setze Dir ein Lesezeichen
                     auf die aktuelle Seite.
-                    @if ($participant->email)<span class="block mt-2">Alternativ erhälst Du den QR-Code ebenfalls an Deine angegebene E-Mail Adresse.</span>@endif
+                    @if ($participant->email)<span class="block mt-2">Alternativ erhälst Du den QR-Code ebenfalls an Deine angegebene E-Mail Adresse und kannst dort jederzeit darauf zugreifen..</span>@endif
                 </div>
             </div>
         </div>
