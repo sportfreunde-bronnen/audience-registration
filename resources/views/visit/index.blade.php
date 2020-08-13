c1d14bfd1c68159f2116af1f46b01e7c725abde9
Magnus Buk <info@magnusbuk.de>
2020-08-13 11:13:21 +0200

    minor hint adaption

--- resources/views/visit/index.blade.php
+++ resources/views/visit/index.blade.php
@@ -23,7 +23,7 @@
                 </div>
                 <div class="text-center text-xs text-gray-600 mt-3">
                     <b>Achtung:</b> Nur durch Vorzeigen des QR-Codes können wir Deinen Einlass garantieren. Speichere ihn daher auf Deinem Handy ab oder setze Dir ein Lesezeichen
-                    auf die aktuelle Seite.
+                    auf die aktuelle Seite. Im Notfall kannst Du diese Seite auch ausdrucken.
                     @if ($participant->email)<span class="block mt-2">Alternativ erhälst Du den QR-Code ebenfalls an Deine angegebene E-Mail Adresse und kannst dort jederzeit darauf zugreifen..</span>@endif
                 </div>
             </div>
