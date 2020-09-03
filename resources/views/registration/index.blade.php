7254d140386b781b4bae5b61c13d1cc0afc11303
Magnus Buk <info@magnusbuk.de>
2020-09-03 10:29:00 +0200

    fix

--- resources/views/registration/index.blade.php
+++ resources/views/registration/index.blade.php
@@ -64,7 +64,7 @@
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="txt-phone">
                             Telefonnummer
                         </label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('email') border-red-500 @enderror" id="txt-phone" name="phone" type="text" placeholder="Telefonnummer">
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('email') border-red-500 @enderror" id="txt-phone" name="phone" type="tel" placeholder="Telefonnummer">
                     </div>
                     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="txt-email">
