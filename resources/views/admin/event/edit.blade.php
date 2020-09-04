bde6808688eea8ac823846a5031c4066361bbac3
Magnus Buk <info@magnusbuk.de>
2020-09-04 13:49:30 +0200

    minor validation fix

--- resources/views/admin/event/edit.blade.php
+++ resources/views/admin/event/edit.blade.php
@@ -38,10 +38,10 @@
                                 @csrf
                                 <div class="flex flex-wrap -mx-3 mb-2">
                                     <div class="w-full px-3 mb-6">
-                                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="txt-amount">
+                                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 @error('name') text-red-500 @enderror" for="txt-amount">
                                             Name der Veranstaltung
                                         </label>
-                                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="txt-name" name="name" type="text" value="{{ old('name', $event->name) }}">
+                                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('name') border-red-500 @enderror" id="txt-name" name="name" type="text" value="{{ old('name', $event->name) }}">
                                     </div>
                                 </div>
                                 <div class="flex flex-wrap -mx-3 md:mb-6">
